f1c360ad6268b9f06f9e854fb627e83436a33ebf
disco-defense <disco.defense@gmail.com>
2024-10-22 15:33:12 +1030

    Add files via upload

---
+++ arch/arch.py
@@ -0,0 +1,80 @@
+'''VGG11/13/16/19 in Pytorch.'''
+import torch
+import torch.nn as nn
+
+
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG(nn.Module):
+    def __init__(self, vgg_name):
+        super(VGG, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 10)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+    
+class MNIST(nn.Module):
+    def __init__(self):
+        super(MNIST, self).__init__()
+        self.features = self._make_layers()
+        self.fc1 = nn.Linear(1024,200)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(200,200)
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc3 = nn.Linear(200,10)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.fc3(out)
+        return out
+
+    def _make_layers(self):
+        layers=[]
+        in_channels= 1
+        layers += [nn.Conv2d(in_channels, 32, kernel_size=3),
+                   nn.BatchNorm2d(32),
+                   nn.ReLU()]
+        layers += [nn.Conv2d(32, 32, kernel_size=3),
+                   nn.BatchNorm2d(32),
+                   nn.ReLU()]
+        layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        layers += [nn.Conv2d(32, 64, kernel_size=3),
+                   nn.BatchNorm2d(64),
+                   nn.ReLU()]
+        layers += [nn.Conv2d(64, 64, kernel_size=3),
+                   nn.BatchNorm2d(64),
+                   nn.ReLU()]
+        layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        
+        return nn.Sequential(*layers)
